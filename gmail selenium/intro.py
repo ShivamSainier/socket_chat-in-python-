275fff8ccf9cdd2552f82813ada881e0eff8c2ff
shivam007-ss <ss1907364@gmail.com>
2020-03-03 10:28:46 +0530

    Right Click Action

--- gmail selenium/intro.py
+++ gmail selenium/intro.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
 import time
 import requests
 import bs4 as bs
@@ -7,4 +8,7 @@ import bs4 as bs
 driver=webdriver.Chrome()
 driver.maximize_window()
 driver.get('https://www.youtube.com/')
-driver.save_screenshot('sc.png')
+button=driver.find_element_by_xpath("//*[@id='logo-icon-container']")
+action=ActionChains(driver)
+action.context_click(button).perform() #mouse right click on youtube logo
+
